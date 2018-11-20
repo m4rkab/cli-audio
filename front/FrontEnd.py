8da01e36385d8e8aeacf83df36314e650ed7d31c
Emma <42856700+m4rkab@users.noreply.github.com>
2018-11-20 12:47:46 -0500

    Update FrontEnd.py

--- front/FrontEnd.py
+++ front/FrontEnd.py
@@ -6,6 +6,11 @@ import os
 import sys
 
 class FrontEnd:
+    # the front end of the application that the user interacts with.
+    # it allows for a menu and interacting with songs.
+    
+    # Attributes:
+    # player - a player object that gets used to control where the song is playing, paused, etc
 
     def __init__(self, player):
         self.player = player
@@ -30,6 +35,10 @@ class FrontEnd:
         self.stdscr.addstr(9,10, "ESC - Quit")
         self.updateSong()
         self.stdscr.refresh()
+        
+        # while the true boolean is true, the menu allows for
+        # certain actions to be taken when the corresponding key is pressed. the first line in
+        # the while loop pulls the value associated with ESC key: 27 is ESC in ascii
                 
         while True:
             c = self.stdscr.getch()
@@ -42,12 +51,21 @@ class FrontEnd:
                 self.updateSong()
                 self.stdscr.touchwin()
                 self.stdscr.refresh()
+            elif c == ord('l'):
+                songlist = self.player.list()
+                x = 0
+                for s in songlist:
+                    self.stdscr.addstr(17 + x, 13, s)
+                    x = x + 1
+                    self.stdscr.refresh()
     
     def updateSong(self):
+        # displays the new song on the menu
         self.stdscr.addstr(15,10, "                                        ")
         self.stdscr.addstr(15,10, "Now playing: " + self.player.getCurrentSong())
 
     def changeSong(self):
+        # asks for the user to input the file path and plays the song
         changeWindow = curses.newwin(5, 40, 5, 50)
         changeWindow.border()
         changeWindow.addstr(0,0, "What is the file path?", curses.A_REVERSE)
