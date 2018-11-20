6695626284beff9c8357a996c6ce2aa8d10faf54
Emma <42856700+m4rkab@users.noreply.github.com>
2018-11-20 12:32:06 -0500

    Update FrontEnd.py

--- front/FrontEnd.py
+++ front/FrontEnd.py
@@ -18,7 +18,7 @@ class FrontEnd:
         try:
             height, width = self.stdscr.getmaxyx()
             if (height < 20 or width < 100):
-                raise CLI_Screen_Size_Exception
+                raise CLI_Audio_Exception.CLI_Screen_Size_Exception
             else:
                 self.stdscr.border()
                 self.stdscr.addstr(0,0, "cli-audio",curses.A_REVERSE)
