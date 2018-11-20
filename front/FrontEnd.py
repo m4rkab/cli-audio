33cc56282b95920de3d2d3bafe814081fcfa1e9a
Emma <42856700+m4rkab@users.noreply.github.com>
2018-11-20 02:17:38 -0500

    Update FrontEnd.py

--- front/FrontEnd.py
+++ front/FrontEnd.py
@@ -1,11 +1,16 @@
 import curses
 import curses.textpad
+import sys
 import CLI_Audio_Exception
 import os
 
-import sys
 
 class FrontEnd:
+    """The front end of the application that the user interacts with. 
+    Its allows for a menu and interacting with songs
+    
+    Attributes: 
+        player: a player object that gets used to control where the song is playing, paused, etc"""
 
     def __init__(self, player):
         self.player = player
@@ -13,7 +18,10 @@ class FrontEnd:
         curses.wrapper(self.menu)
 
     def menu(self, args):
+        """creates a menu for the player to interact with"""
         self.stdscr = curses.initscr()
+		# try to set window
+		# if the window is too small, print error and quit
             try:
                 height, width = self.stdscr.getmaxyx()
                 if (height < 20 or width < 100):
@@ -21,6 +29,7 @@ class FrontEnd:
             except CLI_Screen_Size_Exception:
                 print("Screen size is too small")
                 self.quit()
+
         self.stdscr.border()
         self.stdscr.addstr(0,0, "cli-audio",curses.A_REVERSE)
         self.stdscr.addstr(5,10, "c - Change current song")
@@ -29,6 +38,10 @@ class FrontEnd:
         self.stdscr.addstr(9,10, "ESC - Quit")
         self.updateSong()
         self.stdscr.refresh()
+
+        # while the True boolean is true, the menu allows for certain actions 
+        # to be taken when the corresponding key is pressed. The first line in 
+        # the while loop pulls the value associated with ESC key: 27 is ESC in ascii
         while True:
             c = self.stdscr.getch()
             if c == 27:
@@ -40,12 +53,22 @@ class FrontEnd:
                 self.updateSong()
                 self.stdscr.touchwin()
                 self.stdscr.refresh()
+            elif c == ord('l'):
+                songlist = self.player.list()
+                x = 0
+                for s in songlist:
+                    self.stdscr.addstr(17 + x, 13, s)
+                    x = x+1
+                    self.stdscr.refresh()
+
     
     def updateSong(self):
+        """displays new the new song on the menu"""
         self.stdscr.addstr(15,10, "                                        ")
         self.stdscr.addstr(15,10, "Now playing: " + self.player.getCurrentSong())
 
     def changeSong(self):
+        """asks for user to input the file path and plays the song"""
         changeWindow = curses.newwin(5, 40, 5, 50)
         changeWindow.border()
         changeWindow.addstr(0,0, "What is the file path?", curses.A_REVERSE)
@@ -57,12 +80,13 @@ class FrontEnd:
         self.stdscr.touchwin()
         self.stdscr.refresh()
         self.player.stop()
-            try:
+             try:
                 self.player.play(path.decode(encoding="utf-8"))
-            except CLI_File_Exception:
+             except CLI_File_Exception:
                 print("File path does not exist")
         
 
     def quit(self):
+        """allows the player to quit"""
         self.player.stop()
-        exit()
+		exit()
