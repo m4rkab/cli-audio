a6479c0273518cbc3b65a6c5963058082e87d64e
Emma <42856700+m4rkab@users.noreply.github.com>
2018-11-20 12:22:32 -0500

    Update Player.py

--- player/Player.py
+++ player/Player.py
@@ -3,25 +3,17 @@
 import pyaudio
 import wave
 import time
-import os
-from pathlib import Path
-import CLI_Audio_Exception
 
 class Player:
-    """Controls the actions of the player, such as pausing, quitting, and playing"""
     def __init__(self):
-        """starts with no song being played"""
         self.currentSong = "Nothing playing."
         self.paused = True
         self.position = 0
-        self.playlist = []
 
     def getCurrentSong(self):
-        """returns the current song"""
         return self.currentSong
 
     def pause(self):
-        """if the song is paused then the stream is stopped. One pressed again, the song continues"""
         if self.paused == False:
             self.paused = True
             self.stream.stop_stream()
@@ -30,18 +22,7 @@ class Player:
             self.stream.start_stream()
 
     def play(self, track):
-        """allows the player """
         self.paused = False
-        # check to see if the track exists
-        # if not, throw audio exception
-        try:
-            if not Path(track).is_file():
-                raise CLI_Audio_Exception
-            except CLI_Audio_Exception:
-                currentSong = "Nothing playing."
-                print("File not found")
-                return 0
-
         self.currentSong = track
         self.wf = wave.open(track, 'rb')
 
@@ -59,29 +40,13 @@ class Player:
         self.stream.start_stream()
 
     def stop(self):
-        try:
-            self.stream.stop_stream()
-            self.stream.close()
-            self.wf.close()
-            self.p.terminate() 
-        except:
-            pass
+        self.stream.stop_stream()
+        self.stream.close()
+        self.wf.close()
 
-        
+        self.p.terminate() 
 
     def callback(self, in_data, frame_count, time_info, status):
         data = self.wf.readframes(frame_count)
         return (data, pyaudio.paContinue)
-
-    def list(self):
-        self.songlist = []
-        mylist = os.listdir("./media")
-
-        for file in mylist:
-            filename, extension = os.path.splitext(file)
-            
-            if extension == ".wav":
-                self.songlist.append(filename)
-            
-        return self.songlist
 
